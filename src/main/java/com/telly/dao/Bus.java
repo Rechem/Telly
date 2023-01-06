62a67bbfdb6965c483e1a13bc8826d816240b6b9
Noufel17 <jn_naili@esi.dz>
2023-01-06 15:28:59 +0100

    added feature : Création d'un traget

---
+++ src/main/java/com/telly/dao/Bus.java
@@ -0,0 +1,56 @@
+package com.telly.dao;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="test")
+public class Bus implements Serializable {
+	
+
+	private static final long serialVersionUID = 5362437768854142524L;
+
+
+	@Id
+	@Column(name="dateleave")
+	private Date dateLeave;
+	
+	
+	@Column(name="datereturn")
+	private Date dateReturn;
+	
+	@Column(name="leavingfrom")
+	private String leaveFrom;
+	
+	@Column(name="goingto")
+	private String goingTo;
+	
+	
+	public Bus() {
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Bus [dateLeave=" + dateLeave + ", dateReturn=" + dateReturn + ", leaveFrom=" + leaveFrom + ", goingTo="
+				+ goingTo + "]";
+	}
+
+
+
+
+
+	
+
+
+
+	
+	
+
+}
